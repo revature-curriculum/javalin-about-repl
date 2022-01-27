bbfe7892ecdd15d1fd8f16fb12fc99227560972a
Ashoka Shringla <ashokashringla@gmail.com>
2022-01-27 11:38:08 -0500

    adding required files

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+import io.javalin.Javalin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Javalin app = Javalin.create().start(4100);
+        
+        app.get("/", ctx -> ctx.result("Hello World!"));
+
+    }
+}
